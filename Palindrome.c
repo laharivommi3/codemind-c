4909dcbe1a5297b3de7f2cb7469420d49299eabc
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-10-13 20:37:15 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int n,rev=0,r;     cin>>n;     int temp=n;     while(n>0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     if(temp==rev)     cout<<"True";     else     cout<<"False"; }

---
+++ Palindrome.c
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,rev=0,r;
+    cin>>n;
+    int temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if(temp==rev)
+    cout<<"True";
+    else
+    cout<<"False";
+}
